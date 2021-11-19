4b322778f2917134b35309c2568c7b3c9cbd9617
sefinehtesfa34 <88246220+sefinehtesfa34@users.noreply.github.com>
2021-11-18 21:34:07 -0800

    Create addLinkedListAtEnd.py

---
+++ addLinkedListAtEnd.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    temp=head
+    if temp:
+        while temp.next:
+            temp=temp.next
+        temp.next=SinglyLinkedListNode(data)
+        return head
+    else:
+        temp.next=SinglyLinkedListNode(data)
+        return head
+
+        
+
+if __name__ == '__main__':
